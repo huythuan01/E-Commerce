f3f459a030b4e86366c8763e2835552dbe14aa32
huythuan01 <huythuanpham012@gmail.com>
2025-01-09 23:24:59 +0700

    Update: use EF update database

--- EmployeeManagement/ServerLibrary/Data/AppDbContext.cs
+++ EmployeeManagement/ServerLibrary/Data/AppDbContext.cs
@@ -8,16 +8,13 @@ using System.Threading.Tasks;
 
 namespace ServerLibrary.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
-        {
             public DbSet<Employee> Employees { get; set; }
             public DbSet<Town> Towns { get; set; }
             public DbSet<GeneralDepartment> GeneralDepartments { get; set; }
             public DbSet<Branch> Branches { get; set; }
             public DbSet<Department> Departments { get; set; }
-            public DbSet<ApplicationException> ApplicationExceptions { get; set; }
-        }
+            public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
